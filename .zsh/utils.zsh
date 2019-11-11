bbd4d03514622941d55de72cc782c79d66c4a3c2
Julio Gori Corradi <jcorradi@bionexo.com>
2019-11-11 14:03:12 -0300

    :fire: Removing legacy helper functions

--- .zsh/utils.zsh
+++ .zsh/utils.zsh
@@ -19,13 +19,6 @@ function z () {
     zip -r "$1".zip "$1"/ -x "*.DS_Store";
 }
 
-# Count files
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-function count_files () {
-    FILES=`ls ./ | grep -v ^l | wc -l | awk '{print $1}'`;
-    printf "[${Gre}INFO${NC}] This directory has $FILES files.${NC}\n";
-}
-
 # Uncompress files
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 function extract () {
@@ -59,20 +52,5 @@ function tree() {
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 function mkd() {
     mkdir -p "$@" && cd "$_";
-}
-
-# Get current host related info.
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-function ii() {
-    printf "You are logged on ${BRed}$HOST\n"
-    printf "\n${BRed}Additionnal information:\n$NC"; uname -a
-    printf "\n${BRed}Diskspace:\n$NC"; df -h / $HOME
-    printf "\n${BRed}Current date:\n$NC"; date
-    printf "\n${BRed}Machine stats:\n$NC"; uptime
-    printf "\n${BRed}Memory stats:\n$NC"; free
-    printf "\n${BRed}Local IP Address:\n$NC"; ip
-    printf "\n${BRed}Lan IP Address:\n$NC"; iplocal
-    printf "\n${BRed}Users logged on:\n$NC"; whoami
-    printf "\n${BRed}Open connections:\n$NC"; netstat -pan --inet;
 }
 
