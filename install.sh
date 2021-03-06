dd257535d897b28e13a16d293d1b7069f687d5be
Júlio Gori Corradi <jgcorradi@gmail.com>
2021-03-05 21:19:41 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,37 +19,73 @@ sudo apt install -y \
     libyaml-dev libxml2-dev libbison-dev libxslt1-dev libreadline-dev \
     libpq-dev \
     zlib1g zlib1g-dev \
-    gawk;
+    gawk \
+    apt-transport-https ca-certificates gnupg
+    terminator;
 
 
-#  Install shell packages
+#  Install base packages
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+[ ! -x "$(command -v curl)" ] && sudo apt install curl -y;
 [ ! -x "$(command -v htop)" ] && sudo apt install htop -y;
 [ ! -x "$(command -v git)" ] && sudo apt install git -y;
 [ ! -x "$(command -v vim)" ] && sudo apt install vim -y;
 [ ! -x "$(command -v zsh)" ] && sudo apt install zsh -y;
-[ ! -x "$(command -v curl)" ] && sudo apt install curl -y;
-[ ! -x "$(command -v vim)" ] && sudo apt install vim -y;
 [ ! -x "$(command -v zplug)" ] && curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh;
 [ ! -x "$(command -v fzy)" ] && sudo apt install fzy -y;
 
 
-#  Install containers stack
+#  Docker
+#  [ ! -x "$(command -v docker)" ] && sudo apt install docker.io -y;
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-[ ! -x "$(command -v docker)" ] && sudo apt install docker.io -y;
-sudo usermod -aG docker $USER;
+if [ ! -x "$(command -v docker)" ]; then
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg;
 
-[ ! -x "$(command -v kubectl)" ] && sudo snap install kubectl --channel=1.15/stable --classic;
+  echo \
+    "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null;
+
+ sudo apt-get update;
+ sudo apt-get install docker-ce docker-ce-cli containerd.io;
+ sudo usermod -aG docker $USER;
+fi;
 
 
-#  Install AWS CLI
+#  Docker compose
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-cd $HOME/Desktop;
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip";
-unzip awscliv2.zip;
-sudo ./aws/install;
-rm -r ./awscliv2.zip ./aws;
-cd $HOME;
+if [ ! -x "$(command -v docker-compose)" ]; then
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
+  sudo chmod +x /usr/local/bin/docker-compose;
+fi;
+
+
+#  Kubernetes kubectl
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+[ ! -x "$(command -v kubectl)" ] && sudo snap install kubectl --classic;
+
+
+#  AWS CLI
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+if [ ! -x "$(command -v aws)" ]; then
+  cd $HOME/Desktop;
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip";
+  unzip awscliv2.zip;
+  sudo ./aws/install;
+  rm -r ./awscliv2.zip ./aws;
+  cd $OLDPWD;
+fi;
+
+
+#  Google Chrome
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+if [ ! -x "$(command -v google-chrome)" ]; then
+  CHROME_PACK="google-chrome-stable_current_amd64.deb";
+  cd $HOME/Desktop;
+  wget "https://dl.google.com/linux/direct/$CHROME_PACK"
+  sudo apt install "./$CHROME_PACK"
+  rm "./$CHROME_PACK"
+  cd $OLDPWD;
+fi;
 
 
 #  Create directories arch
@@ -85,6 +121,5 @@ vim +PluginInstall +qall;
 
 #  Set ZSH as default Shell
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo "Setting ZSH as default shell. Prepare to insert your password in 5 seconds!!!"
-sleep 5;
-chsh -s $(which zsh);
+echo "Done!!";
+echo "Please, run the chsh -s \$(which zsh) to set ZSH as default shell and restart the machine!";
