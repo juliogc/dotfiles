2772f81b2453ec204ffe47188f1aa9a2ef90dc25
Julio Gori Corradi <julio.corradi@bhub.ai>
2024-05-24 16:17:30 -0300

    [nvim] Fix none-ls issues

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,10 +1,15 @@
 return {
 	"nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
 	config = function()
 		local null_ls = require("null-ls")
 
 		null_ls.setup({
 			sources = {
+        require("none-ls.diagnostics.eslint_d"),
+
 				null_ls.builtins.formatting.stylua,
 
 				null_ls.builtins.formatting.black,
