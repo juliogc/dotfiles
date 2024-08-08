3a4ed52dd83c98545ab4d2f787eea320afb05eab
Julio Gori Corradi <julio.corradi@bhub.ai>
2024-08-08 19:04:36 -0300

    [fzf] improve config

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -49,11 +49,17 @@ fi;
 
 # FZF
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-export FZF_DEFAULT_OPTS="--layout reverse
+export FZF_DEFAULT_OPTS='--layout reverse
   --border
-  --preview=\"bat --color=always --style=header,grid,changes {}\"
+  --preview="bat --style=header,grid,changes {}"
   --bind shift-up:preview-page-up,shift-down:preview-page-down
-  --color=fg:-1,fg+:#cfc9c2,bg:-1,bg+:#24283b
+  --color=fg:-1,fg+:#cfc9c2,bg:-1,bg+:#24283b,gutter:-1
   --color=hl:#5f87af,hl+:#5fd7ff,info:#e0af68,marker:#9ece6a
   --color=prompt:#f7768e,spinner:#bb9af7,pointer:#bb9af7,header:#b4f9f8
-  --color=border:#24283b,label:#cfc9c2,query:#cfc9c2"
+  --color=border:#24283b,label:#cfc9c2,query:#cfc9c2
+  --prompt="> "
+  --marker=">"
+  --pointer="◆"
+  --separator="─"
+  --scrollbar="│"'
+
