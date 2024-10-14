7903f66a8c8646478dc445bc3193508f61015c48
Julio Gori Corradi <julio.corradi@bhub.ai>
2024-10-14 17:18:49 -0300

    [ZSH Utils] Implements shell decoder for JWT tokens

--- .zsh/utils.zsh
+++ .zsh/utils.zsh
@@ -47,3 +47,9 @@ function extract () {
 function mkd() {
     mkdir -p "$@" && cd "$_";
 }
+
+#  JWT decoder
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+jwt-decode() {
+  jq -R 'split(".") |.[0:2] | map(gsub("-"; "+") | gsub("_"; "/") | gsub("%3D"; "=") | @base64d) | map(fromjson)' <<< $1
+}
