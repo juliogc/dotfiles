c418a58755d9777c5c024b780af9a9551ab63391
Julio Gori Corradi <julio.corradi@bhub.ai>
2025-07-04 21:31:36 -0300

    Adding tl_ls

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,27 @@
-require("nvchad.configs.lspconfig").defaults()
+local nvconfig = require("nvchad.configs.lspconfig")
+local lspconfig = require("lspconfig")
 
-local servers = { "html", "cssls" }
+local servers = {
+  "html",
+  "cssls",
+  "ts_ls",
+  "eslint"
+}
+
+nvconfig.defaults()
 vim.lsp.enable(servers)
+
+
+lspconfig.ts_ls.setup({
+  on_attach = nvconfig.on_attach,
+  capabilities = nvconfig.capabilties,
+  init_options = {
+    preferences = {
+      disableSuggestions = true,
+    }
+  }
+})
+
+lspconfig.eslint.setup({})
 
 -- read :h vim.lsp.config for changing options of lsp servers 
